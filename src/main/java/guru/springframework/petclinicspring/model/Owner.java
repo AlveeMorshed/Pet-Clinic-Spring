1ffbc4c97bb742d09fd95ca6a627fc00e8792551
AlveeMorshed <56661935+AlveeMorshed@users.noreply.github.com>
2022-08-11 02:12:40 +0600

    POJOs added. Closes #2

---
+++ src/main/java/guru/springframework/petclinicspring/model/Owner.java
@@ -0,0 +1,4 @@
+package guru.springframework.petclinicspring.model;
+
+public class Owner extends Person{
+}
---
+++ src/main/java/guru/springframework/petclinicspring/model/Vet.java
@@ -0,0 +1,5 @@
+package guru.springframework.petclinicspring.model;
+
+public class Vet extends Person{
+
+}
